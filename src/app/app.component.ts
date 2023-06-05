349f561a39646deaa907e70e8f87479a0129e27b
SebastianG302 <johan.guerrero@pi.edu.co>
2023-06-04 23:31:14 -0500

    bugs fixed and projects added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent {
       name: "Country App",
       description: "A country finder made with Angular using the RestCountriesApi.",
       img: "../assets/img/imagen-countryapp.png",
+      alt: "Image of a country finder made with Angular using the RestCountriesApi.",
       keyWords: "angular, Bootstrap, api",
       link: "https://bucolic-sunflower-2cf0e7.netlify.app/"
     },
@@ -19,6 +20,7 @@ export class AppComponent {
       name: "Pipes Cheatsheet",
       description: "Angular pipes cheatsheet made with Angular and primeng.",
       img: "../assets/img/pipesimage.png",
+      alt: "Image of Angular pipes cheatsheet made with Angular and primeng.",
       keyWords: "angular, primeng",
       link: "https://silly-cobbler-879619.netlify.app/"
     },
@@ -26,6 +28,7 @@ export class AppComponent {
       name: "Instagram clone",
       description: "My Instagram profile page cloned with Angular.",
       img: "../assets/img/imagen-instagram.png",
+      alt: "Image of my Instagram profile page cloned with Angular.",
       keyWords: "angular, css",
       link: "https://rainbow-figolla-d42e29.netlify.app/"
     },
@@ -33,29 +36,41 @@ export class AppComponent {
       name: "Grade Calculator",
       description: "Percentage grade calculator made with Angular.",
       img: "../assets/img/calculadoradenotas.png",
+      alt: "Image of percentage grade calculator made with Angular.",
       keyWords: "Angular, Bootstrap, api",
       link: "https://notasporporcentajes.000webhostapp.com/"
     },
     {
       name: "Survey",
-      description: "A survey example made in the Responsive Web Desing in freeCodeCamp course.",
+      description: "A survey made in the Responsive Web Desing in freeCodeCamp.",
       img: "../assets/img/imagen-encuesta.png",
+      alt: "Image of a survey example made in the Responsive Web Desing in freeCodeCamp course.",
       keyWords: "html, css",
-      link: "#"
+      link: "https://survey-page-fcc.netlify.app"
     },
     {
       name: "JS Documentation",
       description: "JS basic docs made in the Responsive Web Desing in freeCodeCamp course.",
       img: "../assets/img/imagen-documentacion.png",
+      alt: "Image of JS basic docs made in the Responsive Web Desing in freeCodeCamp course.",
       keyWords: "html, css",
-      link: "#"
+      link: "https://documentation-js-fcc.netlify.app"
     },
     {
       name: "Tribute Page",
       description: "A basic page made in the Responsive Web Desing in freeCodeCamp course.",
       img: "../assets/img/imagen-tributo.png",
+      alt: "Image of a basic page made in the Responsive Web Desing in freeCodeCamp course.",
       keyWords: "html, css",
-      link: "#"
+      link: "https://fcc3-tribute-page.netlify.app"
     },
+    {
+      name: "Landing Page",
+      description: "A simple landing page from freeCodeCamp course.",
+      img: "../assets/img/landing.png",
+      alt: "Image of a simple landing page project",
+      keywords: "html, css",
+      link: "https://landing-pagefcc.netlify.app"
+    }
   ]
 }
